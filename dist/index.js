c2b1a4c7809e30b58bd890599e34b899ef67c1c2
Pedro Palhari <pedropalhari@gmail.com>
2021-01-23 00:58:34 -0300

    feat: Adding demo dist

---
+++ dist/index.js
@@ -0,0 +1,10 @@
+var jsxRuntime = require('react/jsx-runtime');
+
+function Component() {
+  return jsxRuntime.jsx("div", {
+    children: "My Component"
+  }, void 0);
+}
+
+exports.Component = Component;
+//# sourceMappingURL=index.js.map
---
+++ dist/index.modern.js
@@ -0,0 +1,10 @@
+import { jsx } from 'react/jsx-runtime';
+
+function Component() {
+  return jsx("div", {
+    children: "My Component"
+  }, void 0);
+}
+
+export { Component };
+//# sourceMappingURL=index.modern.js.map
